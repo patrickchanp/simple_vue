d7dc1c7614d3d8fb747383b7158a713f70aca9da
Pengfei Qian <pengfei.qian@dentsplysirona.com>
2023-02-21 23:54:26 +0800

    import antd

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import router from "./router";
 import './style.css'
+import Antd from "ant-design-vue"
+import 'ant-design-vue/dist/antd.css';
 import App from './App.vue'
 
 const app = createApp(App);
-app.use(router).mount("#app");
+app.use(router).use(Antd).mount("#app");
