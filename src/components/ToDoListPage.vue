ddc25a85f3b7a7249dfe12b9c15a1f6ffedc8ff0
Pengfei Qian <pengfei.qian@dentsplysirona.com>
2023-02-28 01:49:45 +0800

    add date picker

--- src/components/ToDoListPage.vue
+++ src/components/ToDoListPage.vue
@@ -7,9 +7,14 @@
   <a-list v-if="listRef.length">
     <a-list-item v-for="(item,index) of listRef" :id="item.id" :key="item.id">
         <div v-if="item.isEdit===false" :class="item.status===true?'done':''">
-          <a-checkbox v-model:checked="item.status" @click="handleStatusChange(index)"/>  {{item.title}}
+          <a-checkbox v-model:checked="item.status" @click="handleStatusChange(index)" /> {{item.title}}
+          <a-date-picker v-model:value="item.deadline" :disabled-date="disabledDate" size="small" placeholder="Deadline"/>
         </div>
-        <input v-else v-model="item.title" placeholder="edit todo"/>
+      <div v-else class="edit-todo-item">
+        <a-input class="edit-input" v-model:value="item.title" placeholder="edit todo" :bordered="true" size="small"/>
+        <a-date-picker v-model:value="item.deadline" :disabled-date="disabledDate" size="small" placeholder="Deadline"/>
+      </div>
+
       <div>
         <a-divider type="vertical"/>
         <EditFilled v-if="item.isEdit===false && item.status===false" @click="handleEdit(index)" />
@@ -25,13 +30,14 @@
 <script lang="ts">
 import { reactive, ref} from "vue";
 import {DeleteFilled,EditFilled,CheckOutlined} from "@ant-design/icons-vue";
-
+import dayjs, { Dayjs } from 'dayjs';
 
 interface TodoList {
   id:number,
   title:string,
   isEdit:boolean,
   status:boolean,
+  deadline:dayjs,
 }
 export default {
   name: "ToDoListPage",
@@ -42,48 +48,56 @@ export default {
   let todoItem = ref<string | null>(null);
   let listRef = reactive<Array<TodoList>>([]);
   let idRef = ref<number>(0);
+  let deadlineRef = ref<Dayjs>(null);
 
    const addTodo = () => {
      if(!todoItem.value) return alert("Please finish todo");
      if(listRef.find((item)=>item.title==todoItem.value)){
        return alert("Todo is exist");
-     };
+     }
      listRef.push({
        title:todoItem.value,
        id:++idRef.value,
        isEdit:false,
        status:false,
+       deadline:deadlineRef.value
      });
      todoItem.value="";
+     deadlineRef.value=null;
    }
    const removeItem = (index:number) => {
      listRef.splice(index,1);
    }
-
-    const handleEdit =(index)=>{
+   const handleEdit =(index)=>{
      listRef[index].isEdit = true;
     }
   const handleStatusChange = (index:number) => {
+     //Todo:can't click checkbox when deadline hasn't been choosed
     listRef[index].status = true;
     listRef[index].isEdit=false;
   }
    const saveEdit=(index)=>{
      if(!listRef[index].title) return alert("Todo can't be null");
-     //Todo:编辑后去重
+     //Todo:verify the edited todoItem is repeated or not
      // if (listRef.find((item)=>item.title==todoItem.value)){
      //   return alert("Item is exist")
      // }
      listRef[index].isEdit=false;
     }
+    const disabledDate = (current: Dayjs) => {
+      return current && current < dayjs().endOf('day');
+    };
 
   return {
     todoItem,
     listRef,
+    deadlineRef,
     addTodo,
     removeItem,
     handleEdit,
     saveEdit,
     handleStatusChange,
+    disabledDate,
   };
   }
 }
@@ -91,6 +105,12 @@ export default {
 </script>
 
 <style lang="scss">
+.edit-todo-item{
+  display: inline;
+  .edit-input{
+    width: 100px;
+  }
+}
 .done {
   text-decoration: line-through;
   color: #ddd;
